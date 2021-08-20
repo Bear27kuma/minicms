403b8c9cb304f44536737452452405397b1e616e
Yuta Okuma <y.dream.nest@icloud.com>
2021-08-20 17:26:53 +0900

    Post詳細（show）ビューの作成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Nette\Utils\Paginator;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(
+    public function boot()
     {
         Paginator::useBootstrap();
     }
